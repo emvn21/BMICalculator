7bbf97adeb7be27fd60bea8479814da7732d94fd
Emmanuel van Berlo <90194491+emvn21@users.noreply.github.com>
2023-09-15 14:44:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-height = input("enter your height in m: ")
-weight = input("enter your weight in kg: ")
-#print(type(height))
-#print(type(weight))
-a = float(height) * float(height)
-b = int(weight)
-result = int(b / a)
-print(result)
+
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+
+b = weight
+result = round(b / height ** 2)
+if result < 18.5:
+    print("Your BMI is," +str(result)+ " you are underweight")
+elif result >= 18.5 and result < 25:
+    print("Your BMI is," +str(result)+ " you are a normal weight")
+elif result >= 25 and result < 30:
+    print("Your BMI is," +str(result)+ " you are slightly weight")
+elif result >= 30 and result < 35:
+    print("Your BMI is," +str(result)+ " you are obese")
+else:
+    print("Your BMI is," +str(result)+ " you are clinically obese")
